9d4877770dca7929b7a5e2b30426482629579848
lizill <lizill@naver.com>
2020-11-12 21:56:37 +0900

    login.php

--- signUp.php
+++ signUp.php
@@ -2,21 +2,21 @@
 $id=$_POST['id'];
 $pw=$_POST['pw'];
 $pwc=$_POST['pwc'];
-$name=$_POST['email'];
+$name=$_POST['name'];
+$email=$_POST['email'];
 
 if($pw!=$pwc) {
     echo "비밀번호와 비밀번호 확인이 서로 다릅니다.";
     echo "<a href=signUp.html>back page</a>";
     exit();
 }
-if($id==NULL || $pw==NULL || $name==NULL || $email==NULL)
-{
+if($id==NULL || $pw==NULL || $name==NULL || $email==NULL) {
     echo "빈 칸을 모두 채워주세요.";
     echo "<a href=signUp.html>back page</a>"
     exit();
 }
 
-$mysqli=mysqli_connect("localhost", "root", "123456789", "test2");
+$mysqli=mysqli_connect("localhost", "root", "oracle", "testlogin");
 
 $check="SELECT * from user_info WHERE userid='$id'";
 $result=$mysqli->query($check);
